54fb358b32daec375f81f59683d1bd339f1bc509
Rain <rainvirenesse@gmail.com>
2026-05-04 18:19:23 +0800

    refactor(app): update root app configuration

--- App.js
+++ App.js
@@ -3,11 +3,24 @@ import { View } from 'react-native';
 
 import AppNavigationNi from './src/navigations';
 
+import rootSaga from './src/app/sagas';
+import configureStore from './src/app/reducers';
+
+import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+
+const { store, runSaga } = configureStore();
+runSaga(rootSaga);
+
 const App = () => {
   return (
-    <View style={{ flex: 1 }}>
-      <AppNavigationNi />
-    </View>
+    <Provider store={store}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+        <View style={{ flex: 1 }}>
+          <AppNavigationNi />
+        </View>
+      {/* </PersistGate> */}
+    </Provider>
   );
 };
 
